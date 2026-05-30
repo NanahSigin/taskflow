0d9b6e8cccdac67a357d312a86ce34625dd76020
Ana Paula Machado Silva <anamachado290506@gmail.com>
2026-05-30 03:04:32 -0300

    alterando e colocando teste para tarefa

--- src/tarefa.py
+++ src/tarefa.py
@@ -7,10 +7,4 @@ class Tarefa:
         self.status = "A Fazer"
 
     def __str__(self):
-        return (
-            f"\nID: {self.id}\n"
-            f"Título: {self.titulo}\n"
-            f"Descrição: {self.descricao}\n"
-            f"Prioridade: {self.prioridade}\n"
-            f"Status: {self.status}\n"
-        )
+        return f"{self.id} - {self.titulo} ({self.status})"
