720d3a7577e8d0ce4ba0d9ba07c25a64ab52cb8f
Ana Paula Machado Silva <anamachado290506@gmail.com>
2026-05-30 03:08:31 -0300

    feat: implementa Gerenciador com CRUD básico de tarefas

--- src/gerenciador.py
+++ src/gerenciador.py
@@ -1,42 +1,24 @@
 from tarefa import Tarefa
 
-
 class Gerenciador:
-
     def __init__(self):
         self.tarefas = []
         self.proximo_id = 1
 
     def criar_tarefa(self, titulo, descricao, prioridade):
-
-        tarefa = Tarefa(
-            self.proximo_id,
-            titulo,
-            descricao,
-            prioridade
-        )
-
+        tarefa = Tarefa(self.proximo_id, titulo, descricao, prioridade)
         self.tarefas.append(tarefa)
         self.proximo_id += 1
-
         return tarefa
 
     def listar_tarefas(self):
         return self.tarefas
 
     def buscar_tarefa(self, id_tarefa):
-
         for tarefa in self.tarefas:
-
             if tarefa.id == id_tarefa:
                 return tarefa
-
         return None
 
     def excluir_tarefa(self, id_tarefa):
-
-        self.tarefas = [
-            tarefa
-            for tarefa in self.tarefas
-            if tarefa.id != id_tarefa
-        ]
+        self.tarefas = [t for t in self.tarefas if t.id != id_tarefa]
